ee2f1157601a1846aa3c65df6606739d1f9482fe
Lily Le <lmlt8180@gmail.com>
2024-10-03 15:35:26 -0400

    add padding to body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased p-4`}
       >
         <Header />
         <Hero />
