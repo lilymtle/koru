5f2baf1ee58914d99df894c4edf8c9ee1a00bb94
Lily Le <lmlt8180@gmail.com>
2024-10-03 17:33:05 -0400

    create and update header component

---
+++ src/app/components/Header/Header.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+// Import component
+import Link from "next/link";
+import MenuNav from "../MenuNav/MenuNav";
+
+const Header = () => {
+
+    return (
+        <header className="bg-transparent flex flex-col items-center flex-nowrap md:flex-row justify-between">
+            <img
+                className="mb-4"
+                src="/images/logos/logo.png"
+                alt="koru logo"
+            />
+            <nav>
+                <MenuNav />
+                <div className="nav__wrapper hidden md:block ">
+                    <ul className="flex gap-4">
+                        <li>
+                            <Link 
+                                href="/"
+                                className="text-sm md:text-base nav-hover-bg">
+                                Home
+                            </Link>
+                        </li>
+                        <li>
+                            <Link 
+                                href="/"
+                                className="text-sm md:text-base nav-hover-bg">
+                                About
+                            </Link>
+                        </li>
+                        <li>
+                            <Link 
+                                href="/"
+                                className="text-sm md:text-base nav-hover-bg">
+                                Login
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
