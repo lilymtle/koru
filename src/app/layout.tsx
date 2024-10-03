1a6dfca7ec4f2cf47fc95f6f2223c88f654f1ab3
Lily Le <lmlt8180@gmail.com>
2024-10-03 17:32:54 -0400

    update globals.css file path

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "./globals.css";
+import "@/styles/globals.css";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
