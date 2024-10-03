7fa529f29de98b167134e774486174854d3c9a3a
Lily Le <lmlt8180@gmail.com>
2024-10-03 15:24:24 -0400

    import global components
    - global components include header, hero, and footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,7 +31,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Header />
+        <Hero />
+
         {children}
+
+        <Footer />
       </body>
     </html>
   );
