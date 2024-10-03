44dcfbc6db0bbebced1424d8eab59956d8b82f53
Lily Le <lmlt8180@gmail.com>
2024-10-03 17:19:44 -0400

    create menu navigation component

---
+++ src/app/components/MenuNav/MenuNav.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import MenuIcon from '@mui/icons-material/Menu';
+
+const MenuNav = () => {
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    return (
+        <div className="block md:hidden">
+            <Button
+                id="basic-button"
+                aria-controls={open ? 'basic-menu' : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? 'true' : undefined}
+                onClick={handleClick}
+            >
+                <MenuIcon sx={{color: "#A9B388"}}/>
+            </Button>
+            <Menu
+                id="basic-menu"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                MenuListProps={{
+                    'aria-labelledby': 'basic-button',
+                }}
+            >
+            <Menu
+                id="basic-menu"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                MenuListProps={{
+                    'aria-labelledby': 'basic-button',
+                }}
+            >
+                <MenuItem 
+                    className="text-sm" 
+                    onClick={handleClose}>Home</MenuItem>
+                <MenuItem 
+                    className="text-sm" 
+                    onClick={handleClose}>About</MenuItem>
+                <MenuItem 
+                    className="text-sm" 
+                    onClick={handleClose}>Login</MenuItem>
+            </Menu>
+            </Menu>
+        </div>
+    );
+};
+
+export default MenuNav;
