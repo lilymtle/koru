e0585029c320fa6d0617ea4ae9be79b2ba87a7f4
Lily Le <lmlt8180@gmail.com>
2024-10-04 01:04:14 -0400

    update styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="flex flex-col items-center">
+    <main id="main" className="flex flex-col items-center p-4">
       Main content placeholder
     </main>
   );
