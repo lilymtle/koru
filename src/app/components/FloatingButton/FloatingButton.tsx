f6b5a732fdd896fd256c78231b91236a5801eeac
Lily Le <lmlt8180@gmail.com>
2024-10-09 17:36:58 -0400

    create floating button

---
+++ src/app/components/FloatingButton/FloatingButton.tsx
@@ -0,0 +1,12 @@
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+
+const FloatingButton = () => {
+    return (
+        <div className="floating-button">
+            <ArrowUpwardIcon
+                sx={{color: "#FFFFFF"}} />
+        </div>
+    );
+};
+
+export default FloatingButton;
