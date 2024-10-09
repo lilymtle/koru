3fb369e7d8a58c350121e0894a66b1446246d079
Lily Le <lmlt8180@gmail.com>
2024-10-09 18:09:34 -0400

    update header
    - update nav link style

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -11,26 +11,19 @@ const Header = () => {
         <header className="bg-transparent w-full flex justify-center text-white p-4">
             <nav>
                 <NavDrawer />
-                <div className="nav__wrapper hidden md:block ">
+                <div className="nav__wrapper hidden md:block">
                     <ul className="flex gap-4">
                         <li>
                             <Link 
                                 href="/"
-                                className="text-sm md:text-base nav-hover-bg">
+                                className="text-sm md:text-lg font-bold uppercase nav-hover-bg">
                                 Home
                             </Link>
                         </li>
                         <li>
                             <Link 
-                                href="/about"
-                                className="text-sm md:text-base nav-hover-bg">
-                                About
-                            </Link>
-                        </li>
-                        <li>
-                            <Link 
                                 href="/login"
-                                className="text-sm md:text-base nav-hover-bg">
+                                className="text-sm md:text-lg font-bold uppercase nav-hover-bg">
                                 Login
                             </Link>
                         </li>
