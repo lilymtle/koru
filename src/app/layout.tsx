81e09066392daff4cd379e8f9c92a85dc322f77c
Lily Le <lmlt8180@gmail.com>
2024-10-09 17:37:05 -0400

    import floating button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "@/styles/globals.css";
 import Footer from "./components/Footer/Footer";
 import Hero from "./components/Hero/Hero";
+import FloatingButton from "./components/FloatingButton/FloatingButton";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -19,6 +20,7 @@ export default function RootLayout({
         className="antialiased">
             <Hero />
             {children}
+            <FloatingButton />
             <Footer />
       </body>
     </html>
