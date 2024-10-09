86bc7a087d9a824fc4795a2d786efc45e047d927
Lily Le <lmlt8180@gmail.com>
2024-10-09 17:20:02 -0400

    update footer
    - add bg colour
    - add logo
    - create footer nav list and items

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -1,11 +1,61 @@
 const Footer = () => {
     return (
-        <footer className="flex flex-row justify-between p-4">
-            <p className="text-xs">
-                Made with ♡ by Lily 
+        <footer className="footer">
+            <img
+                className="w-6/12"
+                src="/assets/logos/logo.png"
+                alt="koru logo" />
+
+            <p className="text-xs self-center">
+                Crafted with ♥ by Lily 
             </p>
+
+            <section className="w-full flex flex-row justify-between mt-4 mb-4">
+                <div className="">
+                    <h4 className="footer__title">
+                        General
+                    </h4>
+
+                    <ul>
+                        <li className="text-xs leading-relaxed">
+                            About
+                        </li>
+                        <li className="text-xs leading-relaxed">
+                            Contact
+                        </li>
+                    </ul>
+                </div>
+
+                <div className="">
+                    <h4 className="footer__title">
+                        Social
+                    </h4>
+
+                    <ul>
+                        <li className="text-xs leading-relaxed">
+                            LinkedIn
+                        </li>
+                        <li className="text-xs leading-relaxed">
+                            GitHub
+                        </li>
+                    </ul>
+                </div>
+
+                <div className="">
+                    <h4 className="footer__title">
+                        Resources
+                    </h4>
+
+                    <ul>
+                        <li className="text-xs leading-relaxed">
+                            MoodCompass
+                        </li>
+                    </ul>
+                </div>
+            </section>
+
             <p className="text-xs">
-                © 2024 Koru
+                © 2024 Koru, All Rights Reserved.
             </p>
         </footer>
     );
