855992bb3b5bc31632eed8bfef71a3e7f7d9d0d3
Lily Le <lmlt8180@gmail.com>
2024-10-11 23:17:01 -0400

    update input and button
    - update placeholders
    - pass props and its values to button component

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -31,7 +31,7 @@ const ContactPage = () => {
                         <input 
                             className={styles["contact__input"]}
                             type="email"
-                            placeholder="Your email address"
+                            placeholder="And your email address?"
                         />
                     </label>
 
@@ -45,7 +45,12 @@ const ContactPage = () => {
                         />
                     </label>
 
-                    <Button />
+                    <Button 
+                        buttonClassName="w-full bg-white text-sm text-gray-500 font-bold uppercase border border-white rounded-3xl px-4 py-2 cursor-pointer hover:bg-transparent hover:text-white hover:border-white hover:border-solid duration-500" 
+                        >
+                            Let's start a conversation!
+                    </Button>
+
                 </form>
             </section>
         </section>
